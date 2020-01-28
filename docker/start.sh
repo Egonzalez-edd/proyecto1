c60ec51d44d1d8e2ceb77e78b1b99ea3bceeccf0
Egonzalez-edd <57640532+Egonzalez-edd@users.noreply.github.com>
2020-01-28 11:47:06 -0600

    Etregable90

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-/usr/local/glassfish4/bin/asadmin start-domain
-/usr/local/glassfish4/bin/asadmin -u admin deploy /practicaSpsEgonzalez.war
-/usr/local/glassfish4/bin/asadmin stop-domain
 /usr/local/glassfish4/bin/asadmin start-domain --verbose
